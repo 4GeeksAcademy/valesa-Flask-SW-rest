6880c472257db4ed8c9482c7a1dbc2ef75871340
Valeria Salas <89941500+Valesa185@users.noreply.github.com>
2023-08-21 05:07:02 +0000

    Backend project completed

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet, People, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,277 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+
+# endpoints de "USER"
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_user():
+    users = User.query.all()
+    users_serialized = list(map(lambda x: x.serialize(), users))
 
+    return jsonify({"msg": 'Realizado con éxito', "users": users_serialized})
+
+@app.route('/user/favorites', methods=['GET'])
+def get_favorites():
+    favorites = User.query.all()
+    favorites_serialized = list(map(lambda x: x.serialize(), favorites))
+
+    return jsonify({"msg": 'Realizado con éxito', "favorites": favorites_serialized})
+
+
+
+# endpoints de "PLANET"
+@app.route('/planet', methods=['GET'])
+def get_planets():
+    planets = Planet.query.all()
+    planets_serialized = list(map(lambda x: x.serialize(), planets))
+
+    return jsonify({"msg": 'Realizado con éxito', "planets": planets_serialized})
+
+@app.route("/planet/<int:planet_id>", methods=['GET'])
+def get_id_planet(planet_id):
+    single_planet = Planet.query.get(planet_id)
+    if single_planet is None:
+        raise APIException(f" No se encontró el siguiente Id {planet_id}", status_code=400)
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "GET /planet_id ",
+        "planet_id": planet_id,
+        "planet_info": single_planet.serialize()
     }
-
     return jsonify(response_body), 200
+
+@app.route('/planet', methods=['POST'])
+def post_planet():
+    body = request.get_json(silent=True)
+    if body is None:
+        raise APIException("Envia info al body, esta vacio", status_code=400)
+    print(body)
+    if "id" not in body:
+        raise APIException("Coloca un ID", status_code=400)
+    if "name" not in body:
+        raise APIException("Escribe un nombre", status_code=400)
+    if "diameter" not in body:
+        raise APIException(
+            "Escribe un diametro", status_code=400)
+    if "gravity" not in body:
+        raise APIException(
+            "Escribe una gravedad", status_code=400)
+    if "climate" not in body:
+        raise APIException("Escribe un clima", status_code=400)
+
+    new_planet = Planet(id=body['id'], name=body['name'], diameter=body['diameter'],
+                        gravity=body['gravity'], climate=body['climate'])
+    db.session.add(new_planet)
+    db.session.commit()
+    return jsonify({"msg": "POST exitoso", "new_planet_info": new_planet.serialize()})
+
+@app.route("/planet", methods=['PUT'])
+def modify_planet():
+    body = request.get_json(silent=True)
+    if "id" not in body:
+        raise APIException("Coloca un ID", status_code=400)
+    if "name" not in body:
+        raise APIException("Escribe un nombre", status_code=400)
+    if "diameter" not in body:
+        raise APIException(
+            "Escribe un diametro", status_code=400)
+    if "gravity" not in body:
+        raise APIException(
+            "Escribe una gravedad", status_code=400)
+    if "climate" not in body:
+        raise APIException("Escribe un clima", status_code=400)
+    single_planet = Planet.query.get(body['id'])
+    single_planet.name = body['name']
+    single_planet.diameter = body['diameter']
+    single_planet.gravity = body['gravity']
+    single_planet.climate = body['climate']
+    db.session.commit()
+    return jsonify({"msg": "PUT exitoso"})
+
+@app.route("/planet/<int:planet_id>", methods=['DELETE'])
+def delete_planet(planet_id):
+    single_planet = Planet.query.get(planet_id)
+    if single_planet is None:
+        raise APIException("No existe este planeta", status_code=400)
+
+    db.session.delete(single_planet)
+    db.session.commit()
+
+    return jsonify({"msg": "DELETE exitoso"})
+
+
+
+# endpoints de "PEOPLE"
+@app.route("/people", methods=['GET'])
+def get_people():
+    people = People.query.all()
+    people_serialized = list(map(lambda x: x.serialize(), people))
+
+    return jsonify({"msg": 'Get exitoso', "people": people_serialized}), 200
+
+@app.route("/people/<int:people_id>", methods=['GET'])
+def get_people_id(people_id):
+    single_people = People.query.get(people_id)
+    if single_people is None:
+        raise APIException(f"No existe {people_id}", status_code=400)
+    response_body = {
+        "msg": "GET /people_id ",
+        "people_id": people_id,
+        "people_info": single_people.serialize()
+    }
+    return jsonify(response_body), 200
+
+@app.route('/people', methods=['POST'])
+def post_people():
+    body = request.get_json(silent=True)
+    if body is None:
+        raise APIException("Debes enviar info en el body", status_code=400)
+    print(body)
+    if "id" not in body:
+        raise APIException("Escribe un ID", status_code=400)
+    if "name" not in body:
+        raise APIException("Escribe un nombre", status_code=400)
+    if "height" not in body:
+        raise APIException("Escribe una altura", status_code=400)
+    if "mass" not in body:
+        raise APIException("Escribe un peso", status_code=400)
+    if "eyes_color" not in body:
+        raise APIException("Escribe un color de ojos", status_code=400)
+
+    new_people = People(id=body['id'], name=body['name'], height=body['height'],
+                        mass=body['mass'], eyes_color=body['eyes_color'])
+    db.session.add(new_people)
+    db.session.commit()
+    return jsonify({"msg": "POST exitoso", "new_people_info": new_people.serialize()})
+
+@app.route("/people", methods=['PUT'])
+def modify_people():
+    body = request.get_json(silent=True)
+    if body is None:
+        raise APIException("Crea un body", status_code=400)
+    if "id" not in body:
+        raise APIException(
+            "Ingresa el id nuevo", status_code=400)
+    if "name" not in body:
+        raise APIException("ingresa el nombre nuevo", status_code=400)
+    if "height" not in body:
+        raise APIException(
+            "Ingresa la nueva altura", status_code=400)
+    if "mass" not in body:
+        raise APIException("Ingresa la nueva masa", status_code=400)
+    if "eyes_color" not in body:
+        raise APIException("Ingresa el nuevo color de ojos")
+    single_people = People.query.get(body['id'])
+    single_people.name = body['name']
+    single_people.height= body['height']
+    single_people.mass = body['mass']
+    single_people.eyes_color = body['eyes_color']
+    db.session.commit()
+    return jsonify({"msg": "PUT exitoso"})
+
+@app.route("/people/<int:people_id>", methods=['DELETE'])
+def delete_people(people_id):
+    single_people = People.query.get(people_id)
+    if single_people is None:
+        raise APIException("No existe el personaje", status_code=400)
+
+    db.session.delete(single_people)
+    db.session.commit()
+
+    return jsonify({"msg": "DELETE exitoso"})
+
+
+
+# endpoints de "FAVORITES"
+@app.route("/favorite/planet/<int:planet_id>", methods=['POST'])
+def add_new_favorite_planet(planet_id):
+    current_user = 1
+    planet = Planet.query.filter_by(id=planet_id).first()
+    if planet is not None:
+        favorite = Favorite.query.filter_by(name=planet.name).first()
+        if favorite:
+            return jsonify({"ok": True, "msg": "Existe el planeta favorito"}), 200
+        body = {
+            "name": planet.name,
+            "user_id": current_user
+        }
+        new_favorite = Favorite.create(body)
+        if new_favorite is not None:
+            return jsonify(new_favorite.serialize()), 201
+        return jsonify({"msg": "Hubo un Error"}), 500
+    return jsonify({
+        "msg": "No se encontro en favoritos el planeta"
+    }), 404
+
+@app.route("/favorite/planet/<int:planet_id>", methods=['DELETE'])
+def delete_favorite_planet(planet_id):
+    current_user = 1
+    planet = Planet.query.filter_by(id=planet_id).first()
+
+    if planet is not None:
+        favorite = Favorite.query.filter_by(
+            name=planet.name, user_id=current_user).first()
+
+        if not favorite:
+            return jsonify({"ok": False, "msg": "El planeta en favoritos no existe"}), 404
+
+        try:
+            db.session.delete(favorite)
+            db.session.commit()
+            return jsonify({"ok": True, "msg": "Se elimino el favorito con exito"}), 200
+        except Exception as error:
+            print(error)
+            db.session.rollback()
+            return jsonify({"msg": "Hubo un Error"}), 500
+    return jsonify({
+        "msg": "No se encontro el planeta"
+    }), 404
+
+@app.route("/favorite/people/<int:people_id>", methods=['POST'])
+def add_new_favorite_people(people_id):
+    current_user = 1
+    people = People.query.filter_by(id=people_id).first()
+    if people is not None:
+        favorite = Favorite.query.filter_by(name=people.name).first()
+        if favorite:
+            return jsonify({"ok": True, "msg": "Existe el personaje favorito"}), 200
+        body = {
+            "name": people.name,
+            "user_id": current_user
+        }
+        new_favorite = Favorite.create(body)
+        if new_favorite is not None:
+            return jsonify(new_favorite.serialize()), 201
+        return jsonify({"msg": "Hubo un error"}), 500
+    return jsonify({
+        "msg": "No se encontro en favoritos el personaje"
+    }), 404
+
+@app.route("/favorite/people/<int:people_id>", methods=['DELETE'])
+def delete_favorite_people(people_id):
+    current_user = 1
+    people = People.query.filter_by(id=people_id).first()
+
+    if people is not None:
+        favorite = Favorite.query.filter_by(
+            name=people.name, user_id=current_user).first()
+
+        if not favorite:
+            return jsonify({"ok": False, "msg": "El personaje en favoritos no existe"}), 404
+
+        try:
+            db.session.delete(favorite)
+            db.session.commit()
+            return jsonify({"ok": True, "msg": "Se elimino el favorito con exito"}), 200
+        except Exception as error:
+            print(error)
+            db.session.rollback()
+            return jsonify({"msg": "Hubo un error"}), 500
+    return jsonify({
+        "msg": "No se encontro el personaje"
+    }), 404
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship("Favorite", backref="user", uselist=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -17,3 +18,66 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    diameter = db.Column(db.Integer)
+    gravity = db.Column(db.String(120))
+    climate = db.Column(db.String(120))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "gravity": self.gravity,
+            "climate": self.climate,
+        }
+    
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    height = db.Column(db.Integer)
+    mass = db.Column(db.Integer)
+    eyes_color = db.Column(db.String(120))
+    planet_id =  db.Column(db.Integer, db.ForeignKey("planet.id"))
+    planet = db.relationship("Planet")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "eyes_color": self.eyes_color,
+        }
+    
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    user_id = db.Column(db.Integer,db.ForeignKey("user.id"))
+    __table_args__= (db.UniqueConstraint(
+        'user_id',
+        'name',
+        name= 'favorite_unique'
+    ),)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "user_id": self.user_id
+        }
+    
+    @classmethod
+    def create(cls, favorite):
+        try:
+            new_favorite = cls(**favorite)
+            db.session.add(new_favorite)
+            db.session.commit()
+            return new_favorite
+        except Exception as error:
+            print(error)
+            db.session.rollback()
+            return None
